9b9eda6cd57161ddc2c5de4a89765d46ae66a089
Collins Kipkemoi <122127866+Heisenberg-x1@users.noreply.github.com>
2023-02-27 23:15:30 +0100

    First day at the java office

---
+++ Cute folder/File1.java
@@ -0,0 +1,7 @@
+class File1{
+    public static void main(String[] args){
+        String name;
+        name = System.console().readLine("Enter your name: ");
+        System.out.println("Hello " + name);
+    }
+}
