21934daa93ecce12613af8d1a46d9e7fdf99ef97
Collins Kipkemoi <122127866+Heisenberg-x1@users.noreply.github.com>
2023-02-28 10:28:54 +0100

    Commit

--- Cute folder/File1.java
+++ Cute folder/File1.java
@@ -1,7 +1,23 @@
 class File1{
     public static void main(String[] args){
-        String name;
-        name = System.console().readLine("Enter your name: ");
-        System.out.println("Hello " + name);
+        //(i) Create a program similar to the previous one, but this time the name of the user should be provided as the first command-line argument. Printing to the standard output is also possible using the following command, which Java programmers usually prefer
+        /*
+         * if(args.length != 1){
+            System.err.println("Invalid argument number");
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+         */
+
+
+         //(ii)The program below should print the half of the numbers 1, 2, 3 and 4.
+        int i;
+         for( i=1;i<=4;i++){
+            System.out.println((i/2.0));
+         }
+         i =4;
+         System.out.println("Altogether " + i + " numbers are printed out");
+
+        
     }
 }
